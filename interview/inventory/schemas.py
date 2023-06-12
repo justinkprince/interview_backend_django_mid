9e043678136a474cd53ce40dcd57e0c753783845
Justin Prince <justinkprince@gmail.com>
2023-06-12 13:43:25 -0400

    Fix 'type' object is not subscriptable error.

--- interview/inventory/schemas.py
+++ interview/inventory/schemas.py
@@ -1,10 +1,10 @@
-
+from typing import List
 from decimal import Decimal
 from pydantic import BaseModel
 
 
 class InventoryMetaData(BaseModel):
     year: int
-    actors: list[str]
+    actors: List[str]
     imdb_rating: Decimal
     rotten_tomatoes_rating: int
